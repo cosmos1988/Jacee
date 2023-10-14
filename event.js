afd2a370dd73c13b80d72918a213ca527610ae09
cosmos1988 <cosmos1988@nate.com>
2023-10-14 19:25:32 +0900

    Update event.js

--- event.js
+++ event.js
@@ -1,7 +1,7 @@
 /**
- * @name Jooice Script Library
+ * @name Jooscript
  * @version 0.0.1
- * @author cosmos1988 <https://github.com/cosmos1988/jooice/>
+ * @author cosmos1988 <https://github.com/cosmos1988/jooscript>
  * @license MIT
  * @copyright Copyright © 2023 <cosmos1988>
  */
