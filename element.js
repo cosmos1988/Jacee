c0d0ae737b2134800544ba2ce2e7e3f6ba939398
cosmos1988 <cosmos1988@nate.com>
2023-10-15 18:56:44 +0900

    Update element.js

--- element.js
+++ element.js
@@ -1,7 +1,7 @@
 /**
- * @name Jooscript
+ * @name Jacee
  * @version 0.0.1
- * @author cosmos1988 <https://github.com/cosmos1988/jooscript>
+ * @author cosmos1988 <https://github.com/cosmos1988/Jacee>
  * @license MIT
  * @copyright Copyright © 2023 <cosmos1988>
  */
